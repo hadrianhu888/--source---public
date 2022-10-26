78b07dc4a7c792bd50842fa7bc9262681fec3053
hh888 <hadrian.hu@gmail.com>
2022-10-26 19:21:48 -0400

    message

--- matlab_start_up.bat
+++ matlab_start_up.bat
@@ -1,4 +1,5 @@
 @rem startups matlab r20xx
+rem starts matlab r2022b 
 start "" "D:\Program Files\Matlab\bin\matlab.exe" 
 @rem use start "" "dir" when calling directories with spaces in the names
 @rem matlab started 
